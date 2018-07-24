d737979b495b221f0a0ed193cb0c4b61b9a53228
Passwd <passwd@mes3hacklab.org>
2018-07-24 16:12:51 +0200

    added Copyrights

--- app/src/main/java/com/numa/emma/numa/classes/Program.java
+++ app/src/main/java/com/numa/emma/numa/classes/Program.java
@@ -1,3 +1,9 @@
+/*
+ * Created by passwd <passwd@mes3hacklab.org> on 2018
+ * Copyright (c) 2018-2020. All rights reserved.
+ * Last modified 7/24/18 4:11 PM
+ */
+
 package com.numa.emma.numa.classes;
 
 import com.numa.emma.numa.utils.MedicineUnit;
